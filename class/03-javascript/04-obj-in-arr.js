545b5df7e14eae34d4a8d0344eb596bac0e95e63
Koras02 <wjdgh0727@gmail.com>
2022-01-04 18:18:03 +0900

    완료

--- class/03-javascript/04-obj-in-arr.js
+++ class/03-javascript/04-obj-in-arr.js
@@ -20,17 +20,30 @@
 
 
 
-// const fruits = [
-//     { number: 1, title: "레드향" },
-//     { number: 2, title: "샤인머스켓" },
-//     { number: 3, title: "산청딸기" },
-//     { number: 4, title: "한라봉" },
-//     { number: 5, title: "사과" },
-//     { number: 6, title: "애플망고" },
-//     { number: 7, title: "딸기" },
-//     { number: 8, title: "천혜향" },
-//     { number: 9, title: "과일선물세트" },
-//     { number: 10, title: "귤" }
-// ]
-// undefined
-// fruits[0].number+" "+ fruits[0].title
+const fruits = [
+    { number: 1, title: "레드향" },
+    { number: 2, title: "샤인머스켓" },
+    { number: 3, title: "산청딸기" },
+    { number: 4, title: "한라봉" },
+    { number: 5, title: "사과" },
+    { number: 6, title: "애플망고" },
+    { number: 7, title: "딸기" },
+    { number: 8, title: "천혜향" },
+    { number: 9, title: "과일선물세트" },
+    { number: 10, title: "귤" }
+]
+undefined
+fruits[0].number+" "+ fruits[0].title
+
+// console.log(fruits[0].number + "." + fruits[0].title);
+
+
+for(i = 0; i < 10; i++)  {
+    console.log(
+        fruits[i].number + "." + fruits[i].title
+    );
+
+}
+
+
+
