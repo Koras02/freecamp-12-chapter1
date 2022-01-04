99e026ae9314da233d400e4c9a4a83cd2ac70a0a
Koras02 <wjdgh0727@gmail.com>
2022-01-04 17:05:22 +0900

    wadad

---
+++ class/03-javascript/04-obj-in-arr.js
@@ -0,0 +1,36 @@
+// const classmates = [
+//     { name: "철수", age: 13, school: "다람쥐초등학교" },
+//     { name: "영희", age: 8, school: "공룡초등학교" },
+//     { name: "훈이", age: 11, school: "거북이초등학교" }
+// ]
+
+// 실시간 인기 검색어
+// const fruits = [
+//     { number: 1, title: "레드향" },
+//     { number: 2, title: "샤인머스켓" },
+//     { number: 3, title: "산청딸기" },
+//     { number: 4, title: "한라봉" },
+//     { number: 5, title: "사과" },
+//     { number: 6, title: "애플망고" },
+//     { number: 7, title: "딸기" },
+//     { number: 8, title: "천혜향" },
+//     { number: 9, title: "과일선물세트" },
+//     { number: 10, title: "귤" }
+// ]
+
+
+
+// const fruits = [
+//     { number: 1, title: "레드향" },
+//     { number: 2, title: "샤인머스켓" },
+//     { number: 3, title: "산청딸기" },
+//     { number: 4, title: "한라봉" },
+//     { number: 5, title: "사과" },
+//     { number: 6, title: "애플망고" },
+//     { number: 7, title: "딸기" },
+//     { number: 8, title: "천혜향" },
+//     { number: 9, title: "과일선물세트" },
+//     { number: 10, title: "귤" }
+// ]
+// undefined
+// fruits[0].number+" "+ fruits[0].title
--- class/03-javascript/연습-02-email-split.js
+++ class/03-javascript/연습-02-email-split.js
@@ -1,27 +1,8 @@
-const email = 'codecamp@gamil.com'
-console.log(email)
-email.includs('@')
-
-email.split('@')
-(2) ['codecamp', 'gamil.com']
-email.includes('@')
-let userMail = email.split('@')[0]
-userMail
-'codecamp'
-let company = email.split('@')[1]
-let maskingMail = []
-maskingMail.push(userMail[1])
-maskingMail.push(userMail[2])
-maskingMail.push(userMail[3])
-maskingMail.push(userMail[0])
-maskingMail
-maskingMail.push('*')
-maskingMail.push('*')
-maskingMail.push('*')
-maskingMail.push('*')
-maskingMail
-maskingMail.join("!")
-maskingMail.join("")
-let userId = maskingMail.join("")
-userId
-userId + "@" + company
+const email = 'codecamp@gmail.com';
+email.includes('@');
+email.split('@');
+let userMail = email.split('@')[0];
+userMail;
+let company = email.split('@')[1];
+let maskingMail = [];
+maskingMail.push(userMail[0]);
