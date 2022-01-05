a59771bd5ee8e0c92a4bc11e7d6923a336c7c3e2
Koras02 <wjdgh0727@gmail.com>
2022-01-05 18:25:16 +0900

    adsadasd

---
+++ cyworld-mento/number.js
@@ -0,0 +1,13 @@
+function numberRandom() {
+    document.getElementById("button").innerText = "";
+    
+    
+
+    
+    for(let i = 0; i < 6; i++) {
+        Math.floor(Math.random() * 100000);
+    }
+    
+
+    
+}
