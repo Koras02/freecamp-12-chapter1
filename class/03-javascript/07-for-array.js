bdcfca09da5469b60f0ea4bdabd668033e2df46b
Koras02 <wjdgh0727@gmail.com>
2022-01-05 16:08:54 +0900

    반복문

---
+++ class/03-javascript/07-for-array.js
@@ -0,0 +1,17 @@
+const fruits = [
+    { number: 1, title: "레드향" },
+    { number: 2, title: "샤인머스켓" },
+    { number: 3, title: "산청딸기" },
+    { number: 4, title: "한라봉" },
+    { number: 5, title: "사과" },
+    { number: 6, title: "애플망고" },
+    { number: 7, title: "딸기" },
+    { number: 8, title: "천혜향" },
+    { number: 9, title: "과일선물세트" },
+    { number: 10, title: "귤" }
+]
+
+for (let i = 0; i < fruits.length; i++) {
+     console.log(fruits[i].number + " " + fruits[i].title);
+
+}
